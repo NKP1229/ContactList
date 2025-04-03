d7d7f41ef6cf309775b17e2e7c6c6576f5a3b668
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-03 13:02:00 -0400

    'Selected Contact View' complete. except step9

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
+import { useState } from 'react';
 import ContactList from './components/ContactList.jsx'
-
+import SelectedContact from "./components/SelectedContact";
 function App() {
-
+  const [selectedContactId, setSelectedContactId] = useState(null);
   return (
     <>
-      <ContactList/>
-        
+      {selectedContactId ? (<div>Selected Contact View</div>) : (<ContactList />)}
     </>
   )
 }
