18736ea9492abd84ec2c837aab8878b20f880b4c
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-03 12:23:10 -0400

    'Fetching Data & useEffect' tab complete

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from 'react';
+import { useEffect } from 'react';
 import ContactRow from "./ContactRow";
 
 const dummyContacts = [
@@ -11,6 +12,21 @@ const dummyContacts = [
 export default function ContactList() {
     const [contacts, setContacts] = useState(dummyContacts)
     // console.log("Contacts: ", contacts)
+    useEffect(()=>{
+        async function fetchContacts() {
+            try {
+              const response = await fetch(
+                "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+              );
+              const result = await response.json();
+              setContacts(result);
+            } catch (error) {
+              console.error(error);
+            }
+          }
+          fetchContacts()
+    },[])
+    console.log("Contacts: ", contacts);
     return ( 
         <table>
           <thead>
